7328c4704693d56247646a542649c712dd0f7285
AmintorDusko <amintor_dusko@hotmail.com>
2023-04-25 17:13:03 -0400

    update initialization parameters

--- pennylane_lightning/src/simulators/lightning_kokkos/StateVectorLKokkos.hpp
+++ pennylane_lightning/src/simulators/lightning_kokkos/StateVectorLKokkos.hpp
@@ -141,12 +141,13 @@ class StateVectorLKokkos
                      Kokkos::MemoryTraits<Kokkos::Unmanaged>>;
 
     StateVectorLKokkos() = delete;
-    StateVectorLKokkos(size_t num_qubits)
+    StateVectorLKokkos(size_t num_qubits,
+                       const Kokkos::InitializationSettings &kokkos_args = {})
         : BaseType{num_qubits}, length_(Util::exp2(num_qubits)) {
         {
             const std::lock_guard<std::mutex> lock(init_mutex_);
             if (!Kokkos::is_initialized()) {
-                Kokkos::initialize();
+                Kokkos::initialize(kokkos_args);
             }
         }
 
@@ -162,8 +163,9 @@ class StateVectorLKokkos
      *
      * @param num_qubits Number of qubits
      */
-    StateVectorLKokkos(std::complex<PrecisionT> *hostdata_, size_t length)
-        : StateVectorLKokkos(Util::log2PerfectPower(length)) {
+    StateVectorLKokkos(std::complex<PrecisionT> *hostdata_, size_t length,
+                       const Kokkos::InitializationSettings &kokkos_args = {})
+        : StateVectorLKokkos(Util::log2PerfectPower(length), kokkos_args) {
         // check if length is a power of 2.
         if (!Util::isPerfectPowerOf2(length)) {
             PL_ABORT(
@@ -180,8 +182,9 @@ class StateVectorLKokkos
      *
      * @param other Another Kokkos state vector
      */
-    StateVectorLKokkos(const StateVectorLKokkos &other)
-        : StateVectorLKokkos(other.getNumQubits()) {
+    StateVectorLKokkos(const StateVectorLKokkos &other,
+                       const Kokkos::InitializationSettings &kokkos_args = {})
+        : StateVectorLKokkos(other.getNumQubits(), kokkos_args) {
         this->DeviceToDevice(other.getData());
     }
 
