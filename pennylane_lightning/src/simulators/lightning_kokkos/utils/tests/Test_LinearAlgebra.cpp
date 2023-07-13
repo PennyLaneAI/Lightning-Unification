033630e897d24b4306e00b016789ce34f3a03021
Vincent Michaud-Rioux <vincent.michaud-rioux@xanadu.ai>
2023-07-13 15:09:05 -0400

    Add kokkos utils tests.

--- pennylane_lightning/src/simulators/lightning_kokkos/utils/tests/Test_LinearAlgebra.cpp
+++ pennylane_lightning/src/simulators/lightning_kokkos/utils/tests/Test_LinearAlgebra.cpp
@@ -5,20 +5,36 @@
 #include "LinearAlgebraKokkos.hpp"
 #include "StateVectorKokkos.hpp"
 #include "TestHelpers.hpp"
+#include "Util.hpp" // exp2
+
 #include <catch2/catch.hpp>
+
+/**
+ * @file
+ *  Tests for functionality:
+ *      - defined in the intermediate base class StateVectorKokkos.
+ *      - shared between all child classes.
+ */
+
+/// @cond DEV
+namespace {
+using namespace Pennylane::Lightning_Kokkos;
+using namespace Pennylane::Util;
+} // namespace
+/// @endcond
 
 TEMPLATE_TEST_CASE("Linear Algebra::SparseMV", "[Linear Algebra]", float,
                    double) {
-    using cp_t = Kokkos::complex<TestType>;
+    using ComplexT = StateVectorKokkos<TestType>::ComplexT;
 
     std::size_t num_qubits = 3;
-    std::size_t data_size = Lightning_Kokkos::Util::exp2(num_qubits);
+    std::size_t data_size = exp2(num_qubits);
 
-    std::vector<cp_t> vectors = {{0.0, 0.0}, {0.0, 0.1}, {0.1, 0.1},
-                                 {0.1, 0.2}, {0.2, 0.2}, {0.3, 0.3},
-                                 {0.3, 0.4}, {0.4, 0.5}};
+    std::vector<ComplexT> vectors = {{0.0, 0.0}, {0.0, 0.1}, {0.1, 0.1},
+                                     {0.1, 0.2}, {0.2, 0.2}, {0.3, 0.3},
+                                     {0.3, 0.4}, {0.4, 0.5}};
 
-    const std::vector<cp_t> result_refs = {
+    const std::vector<ComplexT> result_refs = {
         {0.2, -0.1}, {-0.1, 0.2}, {0.2, 0.1}, {0.1, 0.2},
         {0.7, -0.2}, {-0.1, 0.6}, {0.6, 0.1}, {0.2, 0.7}};
 
@@ -37,13 +53,12 @@ TEMPLATE_TEST_CASE("Linear Algebra::SparseMV", "[Linear Algebra]", float,
     kokkos_vx.HostToDevice(vectors.data(), vectors.size());
 
     SECTION("Testing sparse matrix vector product:") {
-        std::vector<cp_t> result(data_size);
-        Lightning_Kokkos::Util::SparseMV_Kokkos<TestType>(
+        std::vector<ComplexT> result(data_size);
+        Util::SparseMV_Kokkos<TestType>(
             kokkos_vx.getData(), kokkos_vy.getData(), values, indices, indptr);
         kokkos_vy.DeviceToHost(result.data(), result.size());
 
-        for (std::size_t j = 0; j < Lightning_Kokkos::Util::exp2(num_qubits);
-             j++) {
+        for (std::size_t j = 0; j < exp2(num_qubits); j++) {
             CHECK(imag(result[j]) == Approx(imag(result_refs[j])));
             CHECK(real(result[j]) == Approx(real(result_refs[j])));
         }
@@ -52,23 +67,25 @@ TEMPLATE_TEST_CASE("Linear Algebra::SparseMV", "[Linear Algebra]", float,
 
 TEMPLATE_TEST_CASE("Linear Algebra::axpy_Kokkos", "[Linear Algebra]", float,
                    double) {
-    using cp_t = Kokkos::complex<TestType>;
+    using ComplexT = StateVectorKokkos<TestType>::ComplexT;
 
     std::size_t num_qubits = 3;
 
-    std::size_t data_size = Lightning_Kokkos::Util::exp2(num_qubits);
+    std::size_t data_size = exp2(num_qubits);
 
-    cp_t alpha = {2.0, 0.5};
+    ComplexT alpha = {2.0, 0.5};
 
-    std::vector<cp_t> v0 = {{0.0, 0.0}, {0.1, -0.1}, {0.1, 0.1}, {0.2, 0.1},
-                            {0.2, 0.2}, {0.3, 0.3},  {0.4, 0.3}, {0.5, 0.4}};
+    std::vector<ComplexT> v0 = {{0.0, 0.0}, {0.1, -0.1}, {0.1, 0.1},
+                                {0.2, 0.1}, {0.2, 0.2},  {0.3, 0.3},
+                                {0.4, 0.3}, {0.5, 0.4}};
 
-    std::vector<cp_t> v1 = {{-0.1, 0.2}, {0.2, -0.1}, {0.1, 0.2}, {0.2, 0.1},
-                            {-0.2, 0.7}, {0.6, -0.1}, {0.1, 0.6}, {0.7, 0.2}};
+    std::vector<ComplexT> v1 = {{-0.1, 0.2}, {0.2, -0.1}, {0.1, 0.2},
+                                {0.2, 0.1},  {-0.2, 0.7}, {0.6, -0.1},
+                                {0.1, 0.6},  {0.7, 0.2}};
 
-    std::vector<cp_t> result_refs = {{-0.1, 0.2}, {0.45, -0.25}, {0.25, 0.45},
-                                     {0.55, 0.4}, {0.1, 1.2},    {1.05, 0.65},
-                                     {0.75, 1.4}, {1.5, 1.25}};
+    std::vector<ComplexT> result_refs = {
+        {-0.1, 0.2}, {0.45, -0.25}, {0.25, 0.45}, {0.55, 0.4},
+        {0.1, 1.2},  {1.05, 0.65},  {0.75, 1.4},  {1.5, 1.25}};
 
     StateVectorKokkos<TestType> kokkos_v0{num_qubits};
     StateVectorKokkos<TestType> kokkos_v1{num_qubits};
@@ -77,13 +94,12 @@ TEMPLATE_TEST_CASE("Linear Algebra::axpy_Kokkos", "[Linear Algebra]", float,
     kokkos_v1.HostToDevice(v1.data(), v1.size());
 
     SECTION("Testing imag of complex inner product:") {
-        Lightning_Kokkos::Util::axpy_Kokkos<TestType>(
-            alpha, kokkos_v0.getData(), kokkos_v1.getData(), v0.size());
-        std::vector<cp_t> result(data_size);
+        Util::axpy_Kokkos<TestType>(alpha, kokkos_v0.getData(),
+                                    kokkos_v1.getData(), v0.size());
+        std::vector<ComplexT> result(data_size);
         kokkos_v1.DeviceToHost(result.data(), result.size());
 
-        for (std::size_t j = 0; j < Lightning_Kokkos::Util::exp2(num_qubits);
-             j++) {
+        for (std::size_t j = 0; j < exp2(num_qubits); j++) {
             CHECK(imag(result[j]) == Approx(imag(result_refs[j])));
             CHECK(real(result[j]) == Approx(real(result_refs[j])));
         }
