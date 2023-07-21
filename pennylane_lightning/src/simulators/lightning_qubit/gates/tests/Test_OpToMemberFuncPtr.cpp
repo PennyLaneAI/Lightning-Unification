d8c6fff3f1d24bd49ac20d4a28e60958475bf97a
Vincent Michaud-Rioux <vincent.michaud-rioux@xanadu.ai>
2023-07-21 07:12:05 -0700

    Add copyright to files where missing.

--- pennylane_lightning/src/simulators/lightning_qubit/gates/tests/Test_OpToMemberFuncPtr.cpp
+++ pennylane_lightning/src/simulators/lightning_qubit/gates/tests/Test_OpToMemberFuncPtr.cpp
@@ -1,3 +1,16 @@
+// Copyright 2018-2023 Xanadu Quantum Technologies Inc.
+
+// Licensed under the Apache License, Version 2.0 (the License);
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an AS IS BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 #include "Constant.hpp"
 #include "ConstantTestHelpers.hpp" // count_unique, first_elems_of, second_elems_of
 #include "ConstantUtil.hpp" // tuple_to_array, array_has_elem, prepend_to_tuple, lookup
