7be0d96bf6c635c785dd471fc12075b9fc960dc5
Vincent Michaud-Rioux <vincent.michaud-rioux@xanadu.ai>
2023-07-24 14:58:31 -0700

    Revert last change which clang-tidy complains about.

--- pennylane_lightning/src/simulators/lightning_qubit/observables/ObservablesLQubit.hpp
+++ pennylane_lightning/src/simulators/lightning_qubit/observables/ObservablesLQubit.hpp
@@ -231,9 +231,8 @@ struct HamiltonianApplyInPlace<StateVectorLQubitManaged<PrecisionT>, true> {
             if (ex) {
 #pragma omp cancel parallel
                 std::rethrow_exception(ex);
-            } else
+            } else {
 #pragma omp critical
-            {
                 scaleAndAdd(length, ComplexT{1.0, 0.0}, local_sv.data(),
                             sum.data());
             }
