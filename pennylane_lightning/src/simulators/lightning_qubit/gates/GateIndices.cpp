1086dfe9ab9b40fa43aa51724ae42d41eb08f62e
Vincent Michaud-Rioux <vincent.michaud-rioux@xanadu.ai>
2023-07-24 14:25:36 -0700

    Remove usage of <ranges>

--- pennylane_lightning/src/simulators/lightning_qubit/gates/GateIndices.cpp
+++ pennylane_lightning/src/simulators/lightning_qubit/gates/GateIndices.cpp
@@ -11,8 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#include <ranges>
-
 #include "ConstantUtil.hpp"
 #include "GateIndices.hpp"
 #include "Util.hpp" // exp2, maxDecimalForQubit
