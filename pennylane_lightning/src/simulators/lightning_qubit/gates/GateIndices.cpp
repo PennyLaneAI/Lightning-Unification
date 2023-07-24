3c0c1ba1afca4be37a0600784e93dbd358c4e7a1
Amintor Dusko <87949283+AmintorDusko@users.noreply.github.com>
2023-07-24 14:33:11 -0400

    Add wheels' build workflows for LightningQubit (#15)
    * build wheels for linux_x86_64
    
    * Windows x86_64 wheel build
    
    * MacOS x86_64 wheel build
    
    * All wheels
    
    * bump minimal Python version to 3.9
    
    * update Windows to windows-2022
    
    * shortening internal dynamic dispather library names for Windows
    
    * fix no arch workflow
    
    * review changes and more
    
    * update reusable workflow concurrency name
    
    * update python version
    
    * remove upload artifact comment
    
    * update other reusable workflow concurrency name

--- pennylane_lightning/src/simulators/lightning_qubit/gates/GateIndices.cpp
+++ pennylane_lightning/src/simulators/lightning_qubit/gates/GateIndices.cpp
@@ -11,10 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#include <ranges>
-
-#include "ConstantUtil.hpp"
 #include "GateIndices.hpp"
+#include "ConstantUtil.hpp"
 #include "Util.hpp" // exp2, maxDecimalForQubit
 
 namespace Pennylane::LightningQubit::Gates {
